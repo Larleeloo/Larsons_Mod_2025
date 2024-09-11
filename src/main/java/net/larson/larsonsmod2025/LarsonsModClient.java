c38b32073f74b37771eab77b8e4fe2f9d9823210
Larleeloo <140470964+Larleeloo@users.noreply.github.com>
2024-09-10 22:21:07 -0700

    1st

---
+++ src/main/java/net/larson/larsonsmod2025/LarsonsModClient.java
@@ -0,0 +1,10 @@
+package net.larson.larsonsmod2025;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LarsonsModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/larson/larsonsmod2025/Larsons_Mod_2025.java
@@ -0,0 +1,15 @@
+package net.larson.larsonsmod2025;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Larsons_Mod_2025 implements ModInitializer {
+	public static final String MOD_ID = "larsons_mod_2025";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
